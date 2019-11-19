c5e571ff4dea4ff5b999508b69db3c38144ca4e4
Philipp <philipp.hoff@outlook.de>
2019-11-19 20:16:23 +0100

    added velocity

--- hw3d/StarLight.h
+++ hw3d/StarLight.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <cmath>
+#include "Vec3.h"
 #include "Star.h"
 #include "PointLight.h"
 
@@ -8,12 +10,16 @@ public:
 	StarLight(const StarLight& s)
 		:
 		StarLight(s.GetProperties(), s.GetGfx(), s.GetStarClassMap())
-	{}
+	{
+		vel = s.GetVelocity();
+	}
 	StarLight(const std::string& properties, Graphics& gfx, StarClassMap& starMap)
 		:
 		star(properties, gfx, starMap),
-		light(star.GetGfx(), std::get<0>(star.GetEquatorialPosition()), std::get<1>(star.GetEquatorialPosition()), std::get<2>(star.GetEquatorialPosition()), std::get<0>(star.GetColor()), std::get<1>(star.GetColor()), std::get<2>(star.GetColor()), 0.2f)
-	{}
+		light(star.GetGfx(), star.GetEquatorialPosition().x, star.GetEquatorialPosition().y, star.GetEquatorialPosition().z, star.GetColor().x, star.GetColor().y, star.GetColor().z, 0.2f)
+	{
+		vel = CalculateVelocity();
+	}
 	void Draw(DirectX::FXMMATRIX view) const
 	{
 		light.Bind(star.GetGfx(), view);
@@ -31,7 +37,23 @@ public:
 	{
 		return star.GetStarClassMap();
 	}
+	Vec3 GetVelocity() const
+	{
+		return vel;
+	}
+	void Update(float dt)
+	{
+		light.SetPos(light.GetPos() + GetVelocity() * dt);
+	}
+private:
+	Vec3 CalculateVelocity() const
+	{
+		const Vec3 eqPos = star.GetEquatorialPosition();
+		const Vec3 hrdPos = star.GetHRDPosition();
+		return hrdPos - eqPos;
+	}
 private:
 	Star star;
 	PointLight light;
+	Vec3 vel = { 0.0f, 0.0f, 0.0f };
 };
