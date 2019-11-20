ac34a7ef484e92f60ac54f92912538f577a36f05
Philipp <philipp.hoff@outlook.de>
2019-11-20 23:02:26 +0100

    Texture out of resource

--- hw3d/Texture.h
+++ hw3d/Texture.h
@@ -8,10 +8,13 @@ namespace Bind
 	class Texture : public Bindable
 	{
 	public:
+		Texture( Graphics& gfx, int resource, UINT slot = 0);
 		Texture( Graphics& gfx,const std::string& path,UINT slot = 0 );
 		void Bind( Graphics& gfx ) noexcept override;
 		static std::shared_ptr<Texture> Resolve( Graphics& gfx,const std::string& path,UINT slot = 0 );
+		static std::shared_ptr<Texture> Resolve(Graphics& gfx, int res, UINT slot = 0);
 		static std::string GenerateUID( const std::string& path,UINT slot = 0 );
+		static std::string GenerateUID(int res, UINT slot = 0);
 		std::string GetUID() const noexcept override;
 		bool HasAlpha() const noexcept;
 	private:
